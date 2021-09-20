a23fe44ae24e257ba3cf134c15c98ad34a8f1c7e
alananobrega <alana.almeida@ee.ufcg.edu.br>
2021-09-20 09:23:07 -0300

    started unic exercise

---
+++ testes_aula1_home/unico/main.go
@@ -0,0 +1,12 @@
+package main
+
+// Muitas vezes, quando estamos analisando determinada lista, surge-se a necessidade de remover os valores únicos dessa lista. E a essa nova lista de valores gerados, damos o nome de "valores únicos" (Unique).
+// Um exemplo dessa transformação pode ser observado abaixo:
+// Array:  [ 2, 3, 8, 9, 3, 2, 6, 4, 5, 1, 5, 5, 6, 5, 4, 7, 5, 2 ]
+// Unique: [ 2, 3, 8, 9, 6, 4, 5, 1, 7 ]
+// Dito isto, desenvolva um algoritmo que remova os valores repetidos de um array e o retorne para o usuário.
+// Lembre-se os elementos não precisam estar na ordem!
+
+func main() {
+	//s
+}
